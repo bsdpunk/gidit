13828d314b90c5b7400c9ca61251d46306c052e2
bsdpunk <bsdpunk@gmail.com>
2019-10-13 14:39:10 -0500

    ThisAgainAdn

--- gidit.go
+++ gidit.go
@@ -6,19 +6,6 @@ import gidit "github.com/bsdpunk/gidit/lib"
 import "os"
 
 func main() {
-	var args struct {
-		Operation  string `arg:"-o,separate","help:"resize,cat or append"`
-		OutputFile string `arg:"-f,separate","help:"output file"`
-		Width      string `arg:"-w,separate","help:"width"`
-		Height     string `arg:"-h,separate","help:"width"`
-		InputFile  string `arg:"-i,separate","help:"input file"`
-	}
-	args.OutputFile = ""
-	args.InputFile = ""
-	args.Height = ""
-	args.Width = ""
-	args.Operation = "resize"
-	arg.MustParse(&args)
 	switch oper := args.Operation; oper {
 	case "cat":
 		gidit.Gicat(os.Args[len(os.Args)-2], os.Args[len(os.Args)-1])
@@ -28,9 +15,13 @@ func main() {
 		// freebsd, openbsd,
 		// plan9, windows...
 		//fmt.Printf("%s.\n", os)
-		if args.InputFile != "" && args.Width != "" {
+		if len(os.Args) > 1 {
+			if len(os.Args) > 3 {
+				gidit.Gisize(os.Args[len(os.Args)-1], os.Args[1], os.Args[2], "out.png")
+			} else {
 
-			gidit.Gisize(args.InputFile, args.Width, args.Height, args.OutputFile)
+				gidit.Gisize(os.Args[len(os.Args)-1], os.Args[1], "", "out.png")
+			}
 		} else {
 			fmt.Println("Must Specify a png file and a size to scale to, or specify different operation")
 			os.Exit(1)
